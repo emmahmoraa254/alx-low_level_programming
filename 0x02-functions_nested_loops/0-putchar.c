b680cc6f801b831cd1b337d08557f65da21f538e
Emmah Moraa <alphagal254@gmail.com>
2022-09-14 12:09:10 -0700

    functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+/*
+ * File: 0-putchar.c
+ * Auth: Emmah Moraa
+ *
+ */
+
+#include "main.h"
+
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return:0.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
